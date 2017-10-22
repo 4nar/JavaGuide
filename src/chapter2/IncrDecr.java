9480fac16f05c10270915888ba4aa37d3c6ef212
4nar <anar.abetayeva@gmail.com>
2017-10-22 20:56:38 +0600

    Chapter 2 Type conversions in assignments

--- src/chapter2/IncrDecr.java
+++ src/chapter2/IncrDecr.java
@@ -6,16 +6,16 @@ package chapter2;
 public class IncrDecr {
     public static void main(String[] args) {
         int x, y;
-
+        int ch;
         x = 10;
         y = ++x;
 
         System.out.println("x: " + x + ", y: " + y);
 
         x = 10;
-        y = ++x;
+        ch = x++;
 
-        System.out.println("x: " + x + ", y: " + y);
+        System.out.println("x: " + x + ", ch: " + ch);
     }
 
 }
